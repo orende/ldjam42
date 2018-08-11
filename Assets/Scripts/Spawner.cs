7f3cd2f2e16e57eca6ee95ed95442629526d9f6b
Joel Björklund <joel.bjorklund@groplay.se>
2018-08-11 14:47:55 +0200

    player01 and spawn (traying)

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Spawner : MonoBehaviour {
+
+    public GameObject objectToSpawn;
+	// Use this for initialization
+	void Start () {
+        EventTrigger trigger = GetComponent<EventTrigger>();
+    }
+
+    public void SpawnAtPosition(PointerEventData data){
+        Debug.Log("spawn " + data);
+    }
+}
