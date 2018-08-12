201fae3a48af3ed4eca87301ce547b081116222d
Sorceress Feraly <sorceressferaly@gmail.com>
2018-08-12 11:39:17 +0200

    add gamecontroller

--- Assets/Scripts/Trigger_PlayerController.cs
+++ Assets/Scripts/Trigger_PlayerController.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Trigger_PlayerController : MonoBehaviour {
-    //  this.gameObject.GetComponent<BoxCollider>().enabled = false;
-    void OnTriggerStay(Collider other)
-    {
-        Debug.Log("Inside: " + other);
-    }
-
 
     void OnTriggerEnter(Collider other)
     {
@@ -18,22 +12,25 @@ public class Trigger_PlayerController : MonoBehaviour {
         {
             case 0:
                 Debug.Log("CASE 0");
-                Debug.Log("CHECKING TAG EQUIPMENT CONFIRMED");
                 if (other.gameObject.name == "Axe" || other.gameObject.name == "Axe(Clone)") { this.GetComponentInParent<PlayerController_01>().equippedItem = 1; Destroy(other.gameObject); }
                 else if (other.gameObject.name == "Mop" || other.gameObject.name == "Mop(Clone)") { this.GetComponentInParent<PlayerController_01>().equippedItem = 2; Destroy(other.gameObject); }
-                else if (other.gameObject.name == "WallBuilder" || other.gameObject.name == "WallBuilder(Clone)") { this.GetComponentInParent<PlayerController_01>().equippedItem = 3; Destroy(other.gameObject); } 
+                else if (other.gameObject.name == "WallBuilder" || other.gameObject.name == "WallBuilder(Clone)") { this.GetComponentInParent<PlayerController_01>().equippedItem = 3; Destroy(other.gameObject); }
+                this.gameObject.SetActive(false);
                 break;
             case 1:
                 Debug.Log("CASE 1");
                 if (other.gameObject.CompareTag("Destructable")) { Destroy(other.gameObject); }
+                this.gameObject.SetActive(false);
                 break;
             case 2:
                 Debug.Log("CASE 2");
                 if (other.gameObject.CompareTag("Schmooze")) { Destroy(other.gameObject); }
+                this.gameObject.SetActive(false);
                 break;
 
             default:
                 Debug.Log("DEFAULT HAPPENING");
+                this.gameObject.SetActive(false);
                 break;
         }
     }
